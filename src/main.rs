ffad6ace88f491a96e017427ed214d245ccb6955
Sebi <sunipan@gmail.com>
2023-12-29 14:53:01 -0800

    add line count functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use std::fs;
+use std::{
+    fs,
+    io::{self, BufRead},
+};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -29,10 +32,10 @@ fn main() {
     let args = Args::parse();
 
     if args.count {
-        count_file_bytes(args.file_name);
+        count_file_bytes(&args.file_name);
     }
     if args.lines {
-        count_file_lines();
+        count_file_lines(&args.file_name);
     }
     if args.words {
         count_file_words();
@@ -42,17 +45,35 @@ fn main() {
     }
 }
 
-fn count_file_bytes(file_name: String) {
+fn count_file_bytes(file_name: &String) {
     let file_result = fs::read(file_name);
     let file = match file_result {
         Ok(file_vec) => file_vec,
-        Err(error) => panic!("Problem reading error: {:?}", error),
+        Err(error) => {
+            eprintln!("Problem reading error: {:?}", error);
+            return;
+        }
     };
-
     println!("Bytes: {}", file.len());
 }
 
-fn count_file_lines() {}
+fn count_file_lines(file_name: &String) {
+    let mut line_count = 0;
+    let file_result = fs::File::open(file_name);
+    let reader = match file_result {
+        Ok(file) => io::BufReader::new(file),
+        Err(error) => {
+            eprintln!("Problem reading error: {:?}", error);
+            return;
+        }
+    };
+    for line in reader.lines() {
+        if let Ok(_line) = line {
+            line_count += 1;
+        }
+    }
+    println!("Lines: {}", line_count);
+}
 
 fn count_file_words() {}
 
