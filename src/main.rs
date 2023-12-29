a1403726667bbee62e03783fa3a4c342b6dc3d27
Sebi <sunipan@gmail.com>
2023-12-29 13:27:23 -0800

    add cli args, boilerplate

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Option to count bytes of file
+    #[arg(short)]
+    count: bool,
+
+    // Option to count lines of file
+    #[arg(short)]
+    lines: bool,
+
+    // Option to count words of file
+    #[arg(short)]
+    words: bool,
+
+    // Option to count characters of file
+    #[arg(short)]
+    m: bool,
+
+    // File to run operations on
+    file_name: String,
+}
+
+fn main() {
+    let args = Args::parse();
+}
+
+fn count_file_bytes() {}
+
+fn count_file_lines() {}
+
+fn count_file_words() {}
+
+fn count_file_characters() {}
