9b1868b1157e79ab1cfa6d5ea94323e27a25d015
Sebi <sunipan@gmail.com>
2024-01-01 16:41:40 -0800

    remove irrelevant comment

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::{
     io::{self, BufRead, BufReader, Read},
 };
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
